15b0007a2110e575dbfb220dc2fb5c5ae6346ae1
Dolcia4806 <gax36691@student.gdynia.merito.pl>
2026-04-24 19:38:33 +0200

    .

--- Assets/Sprite/UI/KlikaczSurowca.cs
+++ Assets/Sprite/UI/KlikaczSurowca.cs
@@ -27,4 +27,8 @@ public class KlikaczSurowca : MonoBehaviour
             Instantiate(prefabCzasteczki, pozycjaMyszki, Quaternion.identity, miejsceNaCzasteczki);
         }
     }
+    private void OnMouseDown()
+    {
+        KliknietoSurowiec();
+    }
 }
